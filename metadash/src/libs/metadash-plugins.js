dcffecc96c89e756bac2db60d6da3ec677acbc1b
Kairui Song <kasong@redhat.com>
2017-06-06 19:24:52 +0800

    Add lost file

---
+++ metadash/src/libs/metadash-plugins.js
@@ -0,0 +1,13 @@
+// This file will be overrided by plugins-loader
+// See build/plugin-loader.js for more detail
+// Don't rename this file, and changing content have no effect.
+const Plugins = {
+  'plugin-name': {
+    path: 'plugin-routing-name',
+    entry: '<Component>',
+    icon: '<Element>',
+    title: 'Plugin Name'
+  }
+}
+
+export default Plugins
