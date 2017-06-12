f202e8968c546999c7676863e2813699b22415e7
Kairui Song <kasong@redhat.com>
2017-06-12 10:11:09 +0800

    job-trigger: Remove job redirection API

--- metadash/plugins/job-trigger/apis/__init__.py
+++ metadash/plugins/job-trigger/apis/__init__.py
@@ -19,9 +19,3 @@ def trigger_jobs():
     res = requests.post(Config.get("TRIGGER_JOBS_URL"), request.json)
     res.raise_for_status()
     return res.text
-
-
-@app.route("/goto-job/<job_name>", methods=["GET"])
-def goto_job(job_name):
-    return redirect("{}/job/{}".format(
-        Config.get("JENKINS_INSTANCE_URL").rstrip("/"), job_name.lstrip("/")))
