71678d43cfc610a567df818b1b36c3615b25a903
Kairui Song <kasong@redhat.com>
2017-06-12 16:03:21 +0800

    Add lost file

---
+++ metadash/plugins/job-trigger/components/job-card.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <pf-card :title="fullName" :show-titles-separator="false">
+      <a :href="jenkinsUrl + '/job/' + provisionName" target="_blank">Jenkins Provision Page</a>
+      <a :href="jenkinsUrl + '/job/' + runtestName" target="_blank">Jenkins Runtest Page</a>
+      <a :href="jenkinsUrl + '/job/' + teardownName" target="_blank">Jenkins Teardown Page</a>
+      <a class="pull-right" href="javascript:void(0)" @click="deleteJob">Delete from list</a>
+    </pf-card>
+  </div>
+</template>
+
+<script>
+import Dropdown from 'vue-strap/src/Dropdown'
+export default {
+  name: 'job-card',
+  components: { Dropdown },
+  props: [ 'arch', 'product', 'version', 'component', 'jobName', 'jenkinsUrl' ],
+  data () {
+    return {}
+  },
+  methods: {
+    deleteJob () {
+      this.$emit('deleteJob')
+    }
+  },
+  computed: {
+    fullName () {
+      return `${this.component}-${this.product}-${this.version}-${this.arch}-${this.jobName}`
+    },
+    runtestName () {
+      return `${this.component}-${this.product}-${this.version}-runtest-${this.arch}-${this.jobName}`
+    },
+    provisionName () {
+      return `${this.component}-${this.product}-${this.version}-provision-${this.arch}-${this.jobName}`
+    },
+    teardownName () {
+      return `${this.component}-${this.product}-${this.version}-teardown-${this.arch}-${this.jobName}`
+    }
+  },
+  created () {
+  },
+  watch: {
+  }
+}
+</script>
+
+<style scoped>
+a {
+  margin-right: 12px;
+}
+</style>
