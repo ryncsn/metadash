78b7772478aa59691c632dc72ee89ab2467cf68b
ryncsn <ryncsn@gmail.com>
2017-06-26 23:38:04 +0800

    Optimize code and add some draft

--- metadash/src/App.vue
+++ metadash/src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <pf-layout id="app" :icons="true">
-    <pf-notifications ref="notifications">
-    </pf-notifications>
+    <pf-notifications ref="notifications"> </pf-notifications>
+    <pf-drawer :hidden="!showNotificationDrawer" :allow-expand="true" title="Notifications">
+      <pf-drawer-group title="Recent Activities" action="Mark All Read">
+        <pf-drawer-notification message="User xxx triggerd jobs">
+        </pf-drawer-notification>
+      </pf-drawer-group>
+      <pf-drawer-group title="Running tasks">
+        <pf-drawer-notification message="Background Celery tasks will be shown here">
+        </pf-drawer-notification>
+      </pf-drawer-group>
+      <pf-drawer-group title="Server Info">
+        <pf-drawer-notification message="Metadash v0.1 Running">
+        </pf-drawer-notification>
+      </pf-drawer-group>
+    </pf-drawer>
     <bs-modal title="User Info" effect="fade" width="800" :value="showTopModal" @closed="showTopModal = false">
-      <user v-show="loggedIn" slot="modal-body" class="modal-body" @success="showTopModal = false">
-      </user>
-      <login v-show="!loggedIn" slot="modal-body" class="modal-body" @success="showTopModal = false">
-      </login>
+      <user v-show="loggedIn" slot="modal-body" class="modal-body" @success="showTopModal = false"> </user>
+      <login v-show="!loggedIn" slot="modal-body" class="modal-body" @success="showTopModal = false"> </login>
       <div slot="modal-footer">
         <div v-if="loginModalInfo" class="alert alert-danger">
           <span class="pficon pficon-error-circle-o"></span>
@@ -14,11 +25,17 @@
         </div>
       </div>
     </bs-modal>
-    <li slot="utility-menu">
-      <a class="nav-item-iconic" @click="loginModal(true)">
-        <span class="fa fa-user" title="User" data-toggle="modal" data-target=".bs-example-modal-sm"></span>
-      </a>
-    </li>
+    <ul slot="utility-menu" class="nav navbar-nav navbar-right">
+      <li>
+        <a class="nav-item-iconic"> <span title="Reload Current Page" class="fa fa-refresh"></span> </a>
+      </li>
+      <li>
+        <a class="nav-item-iconic" @click="notificationDrawer(!showNotificationDrawer)"> <span title="Notifications" class="fa fa-bell"></span> </a>
+      </li>
+      <li>
+        <a class="nav-item-iconic" @click="userModal(true)"> <span title="User Info" class="fa fa-user"></span> <b>{{ username }}</b> </a>
+      </li>
+    </ul>
     <router-link slot="brand" to="/" :exact="true" class="navbar-brand">
       <span class="navbar-brand-name">Metadash</span>
     </router-link>
@@ -59,20 +76,22 @@ export default {
   components: { Login, User },
   data () {
     return {
+      showNotificationDrawer: false,
       showTopModal: false,
-      showErrorModal: false,
       loginModalInfo: '',
       errorModalInfo: ''
     }
   },
   methods: {
-    loginModal (show, info) {
+    userModal (show, info) {
       this.showTopModal = show
       this.loginModalInfo = info
     },
-    errorNotice (info) {
-      this.showErrorModal = !this.showErrorModal
-      this.loginModalInfo = info
+    notificationDrawer (show, info) {
+      this.showNotificationDrawer = show
+    },
+    makeToast (text, level) {
+      this.$refs.notifications.add(text, level)
     }
   },
   created () {
@@ -80,23 +99,23 @@ export default {
     Vue.http.interceptors.push((request, next) => {
       next((response) => {
         if (response.status === 401) {
-          vm.loginModal(true, "You don'e have required permission")
+          vm.userModal(true, "You don'e have required permission")
           return response
         } else if (response.status === 202) {
           response.json().then((data) => {
-            this.$refs.notifications.add(data.message, 'info')
+            this.makeToast(data.message, 'info')
           }, () => {
-            this.$refs.notifications.add(response.body || 'No Response', 'warning')  // TODO: Danger!!!
+            this.makeToast(response.body || 'No Response', 'warning')  // TODO: Danger!!!
           })
         } else if (!response.ok) {
           response.json().then((data) => {
             if (data.message) {
-              this.$refs.notifications.add(data.message, 'danger')
+              this.makeToast(data.message, 'danger')
             } else {
-              this.$refs.notifications.add('Unknown Error: ' + JSON.stringify(data), 'danger')
+              this.makeToast('Unknown Error: ' + JSON.stringify(data), 'danger')
             }
           }, () => {
-            this.$refs.notifications.add(response.body || 'No Response', 'danger')  // TODO: Danger!!!
+            this.makeToast(response.body || 'No Response', 'danger')  // TODO: Danger!!!
           })
           return response
         }
@@ -109,6 +128,9 @@ export default {
   computed: {
     loggedIn () {
       return !!this.$store.state.username
+    },
+    username () {
+      return this.$store.state.username || 'anonymous'
     }
   }
 }
@@ -144,5 +166,4 @@ Fix for pattern fly side bar animation
   -o-transition:width 300ms ease-in-out, height 300ms ease-in-out;
   transition:width 300ms ease-in-out, height 300ms ease-in-out;
 }
-
 </style>
