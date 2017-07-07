ba4b21cc5b0899803c384a169b96fae37009b3cc
Kairui Song <kasong@redhat.com>
2017-07-07 16:58:12 +0800

    Add webpack alias for plugin path

--- metadash/build/webpack.base.conf.js
+++ metadash/build/webpack.base.conf.js
@@ -29,7 +29,8 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src')
+      '@': resolve('src'),
+      '%': resolve('plugins')
     }
   },
   module: {
