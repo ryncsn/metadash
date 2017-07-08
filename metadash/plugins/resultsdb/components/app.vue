34d6c0ee5d4baef933a5a739feb5d8f658c44a0a
ryncsn <ryncsn@gmail.com>
2017-07-09 00:40:30 +0800

    Test result plugin

---
+++ metadash/plugins/resultsdb/components/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'testresult-navi',
+  data () {
+    return {}
+  },
+  mounted () {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
