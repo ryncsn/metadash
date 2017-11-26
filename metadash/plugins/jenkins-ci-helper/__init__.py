87183a4f01f9451aaa2fa0933c0b7fd9f0b97154
ryncsn <ryncsn@gmail.com>
2017-11-26 22:16:21 +0800

    Remove a plugin

--- metadash/plugins/jenkins-ci-helper/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Example Plugin
-Keep the __init__.py so plugins can access to top package
-
-component/index.js: path have to be uniq among all registed plugins
-plugin.json: name have to be uniq among all plugins
-"""
