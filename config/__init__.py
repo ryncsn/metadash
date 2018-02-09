d094caefb91c3a33025ac7eea14931ef6d1ea671
Kairui Song <kasong@redhat.com>
2018-02-09 18:44:18 +0800

    Load config from enviromental variables
    This make it easier to deploy with containers.
    Also update dependencies

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,15 @@
 """
-Default config file that controls the whole project.
+Default configs and settings
 This file hold the configures that should be only visible at server side
-and should be changed at runtime, like DB backend URL and secret key values.
+and won't be changed at runtime, like DB backend URL and secret key values.
 
-This file is only intend to be used for server setup, for any other configs,
+Any config here could be overrided by:
+
+ - appropriate enviroment variable
+
+ - ./config.py in this folder
+
+This file is only intend to be used for server deployment, for any other configs,
 like user/password/3rd part API url, use Database, or metadash.config.
 """
 
@@ -14,25 +20,37 @@ basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../metadash")
 logger = logging.getLogger(__name__)
 
 
+class Required:
+    def __init__(self, v_type=None):
+        self.v_type = v_type
+
+
 class Config(object):
+    """
+    Modify the configs here to modify the default value
+    """
+    _ENV_PREFIX = 'APP_'
+
     DEBUG = True
     TESTING = False
     SECRET_KEY = ''  # Replace with some random string please
     SECURITY = False
+    DEVELOPMENT = True
     CACHE_DEFAULT_BACKEND = 'dogpile.cache.memory'
     CACHE_ARGUEMENTS = {}
-
-    CELERY_BROKER_URL = ''
-    CELERY_RESULT_BACKEND = ''
-    # CELERY_BROKER_URL = 'redis://localhost:6379'
-    # CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 
     SQLALCHEMY_DATABASE_URI = 'sqlite:///{basedir}/test.db'.format(basedir=basedir)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # CELERY_BROKER_URL = 'redis://localhost:6379'
+    # CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+    CELERY_BROKER_URL = ''
+    CELERY_RESULT_BACKEND = ''
+
 
 class ProductionConfig(Config):
     DEBUG = False
+    DEVELOPMENT = False
 
 
 class DevelopmentConfig(Config):
@@ -44,11 +62,39 @@ class TestingConfig(Config):
     TESTING = True
 
 
+# Override from config.py
 try:
     from .config import ActiveConfig
 except ImportError:
     ActiveConfig = DevelopmentConfig
 
+# Override from Env
+for attr_name in dir(ActiveConfig):
+    if attr_name.startswith('_') or attr_name.upper() != attr_name:
+        continue
+
+    orig_value = getattr(ActiveConfig, attr_name)
+    is_required = isinstance(orig_value, Required)
+    orig_type = orig_value.v_type if is_required else type(orig_value)
+    env_var_name = ActiveConfig._ENV_PREFIX + attr_name
+    env_var = os.getenv(env_var_name, None)
+    if env_var is not None:
+        if issubclass(orig_type, bool):
+            env_var = env_var.upper() in ('1', 'TRUE')
+        elif issubclass(orig_type, int):
+            env_var = int(env_var)
+        elif issubclass(orig_type, bytes):
+            env_var = env_var.encode()
+        # could do floats here and lists etc via json
+        setattr(ActiveConfig, attr_name, env_var)
+    elif is_required:
+        raise RuntimeError('The required environment variable "{0}" is currently not set'
+                           .format(env_var_name))
+
+
+if not ActiveConfig.SECRET_KEY:
+    logger.critical("Please set your non-empty secret value for production!")
+    ActiveConfig.SECRET_KEY = 'ABCDEFG'  # hmmmm... VERY SECRET
 
 # Some common checks
 if not ActiveConfig.SECRET_KEY:
