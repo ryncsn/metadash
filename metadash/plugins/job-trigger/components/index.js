501fb059ca21be3c18326e250f067a23b72d2d60
Kairui Song <kasong@redhat.com>
2018-03-02 23:23:49 +0800

    Remove non-common plugins

--- metadash/plugins/job-trigger/components/index.js
+++
@@ -1,8 +0,0 @@
-import component from './app.vue'
-
-export default {
-  path: '/job-trigger', // Need to be uniq, and will be used for url routing
-  icon: '<i class="fa fa-rocket" aria-hidden="true"></i>', // Or import a webpack resource
-  entry: component,
-  title: 'Job Trigger'
-}
--- metadash/plugins/resultsdb/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Test Result Plugin
-Keep the __init__.py so plugins can access to top package
-"""
