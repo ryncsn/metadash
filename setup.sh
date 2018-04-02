c1c1bf0426cb12ba1685b300256e249aa7fa41e0
Kairui Song <kasong@redhat.com>
2018-04-02 12:49:47 +0800

    setup.sh: detect and call the right python binary

--- setup.sh
+++ setup.sh
@@ -6,22 +6,35 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 _error() {
-    echo -e ${RED}$1${NC}; exit 1;
+    echo -e ${RED}$1${NC} > /dev/stderr; exit 1;
 }
 
 _info() {
-    echo -e ${GREEN}$1${NC}
+    echo -e ${GREEN}$1${NC} > /dev/stdout
 }
 
-function _is_virtualenv_installed () {
-    virtualenv --help &>/dev/null || _error "'virtualenv' is needed but not installed properly"
+function _python () {
+    ($(command -v python3) --version &>/dev/null && (python3 $@; return $?)) ||
+    ($(command -v python) --version &>/dev/null && (python $@; return $?))
 }
 
-function _is_pip_installed () {
-    pip &>/dev/null || _error "'pip' required but not installed properly, exiting"
+function _pip () {
+    _python -m pip $@
 }
 
-function _is_npm_installed () {
+function _virtualenv () {
+    _python -m virtualenv $@
+}
+
+function _ensure_virtualenv_installed () {
+    _virtualenv --help &>/dev/null || _error "'virtualenv' is needed but not installed properly"
+}
+
+function _ensure_pip_installed () {
+    _pip -V &>/dev/null || _error "'pip' required but not installed properly, exiting"
+}
+
+function _ensure_npm_installed () {
     $(command -v npm &> /dev/null) || _error "'npm' is needed but not installed properly"
 }
 
@@ -50,29 +63,31 @@ do
     shift
 done
 
-_is_pip_installed
-_is_npm_installed
+_info "***Make sure required tools are installed***"
+_ensure_pip_installed
+_ensure_npm_installed
 
+_info "***Create virtualenv if needed***"
 if [[ $VENV == 'true' ]] ; then
-    _is_virtualenv_installed
-    virtualenv $VENV_PATH
+    _ensure_virtualenv_installed
+    _virtualenv $VENV_PATH
     source $VENV_PATH/bin/activate
 fi
 
 _info "***Installing requirements of Metadash***"
 if [[ -f 'requirements.txt' ]]; then
-    pip install -r requirements.txt
+    _pip install -r requirements.txt
 fi
 
 if [[ $DEV == 'true' && -f 'requirements.dev.txt' ]]; then
     _info "Installing dev requirements of Metadash"
-    pip install -r requirements.txt
+    _pip install -r requirements.txt
 fi
 
 _info "***Installing requirements of Plugins***"
 for file in ./metadash/plugins/*/requirements.txt; do
     if [[ -f $file ]]; then
-        pip install -r $file
+        _pip install -r $file
     fi
 done
 
