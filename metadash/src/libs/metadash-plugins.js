3fb67085bf90eac2af603de80ab0337f86116f83
Kairui Song <kasong@redhat.com>
2018-04-28 18:18:03 +0800

    Prepare for add router permission control

--- metadash/src/libs/metadash-plugins.js
+++ metadash/src/libs/metadash-plugins.js
@@ -3,10 +3,21 @@
 // Don't rename this file, and changing content have no effect.
 const Plugins = {
   'plugin-name': {
+    // The routing path for a plugin, should be unique
     path: 'plugin-routing-name',
+    // Then main component for the plugin
     entry: '<Component>',
+    // The icon for the plugin, currently need to be a meterial icon name
     icon: '<Element>',
-    title: 'Plugin Name'
+    // Plugin name as it's title
+    title: 'Plugin Name',
+    // Which role is allowed to use this plugin, defaults to all ([anonymous, user, admin])
+    // We only have three role and it's hard coded
+    roles: [
+      'anonymous',
+      'user',
+      'admin'
+    ]
   }
 }
 
