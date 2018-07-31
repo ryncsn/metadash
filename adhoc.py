e549125be074fd7013d1e0a93b1afa7d2c3affdc
Kairui Song <kasong@redhat.com>
2018-07-31 18:08:19 +0800

    Add a adhoc script boilerplate

---
+++ adhoc.py
@@ -0,0 +1,9 @@
+"""
+Add ad hoc script here
+"""
+import metadash
+# Import any submodule you need
+# from metadash.utils.umb import subscribe
+
+print(metadash.__name__)
+# subscribe('metadash-adhoc', 'qe.ci')
