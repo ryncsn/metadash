32d87dcf683d943ba7d1fc7fad15d47a33d8f430
Kairui Song <kasong@redhat.com>
2018-08-22 00:13:28 +0800

    Add a setup.py and make it packagable

--- manager.py
+++ manager.py
@@ -15,147 +15,151 @@ def info(msg):
     print("{}{}{}".format(GREEN, msg, NC))
 
 
-def do_initialize(dev=False, only_dep=False):
+def do_initialize(dev=False, inst_py_dep=True, inst_node_dep=True, build_assert=False):
     try:
-        info("*** Installing requirements of Metadash ... ***")
-        subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
-        if dev:
-            subprocess.run(['pip', 'install', '-r', 'requirements.dev.txt'])
-        info("*** Installing requirements of Metadash Done ***")
+        if not (inst_py_dep or inst_node_dep or build_assert):
+            error("*** There is nothing to do! ***")
+            sys.exit(1)
 
-        info("*** Installing requirements of Metadash Plugins ... ***")
-        for filename in glob.iglob('metadash/plugins/*/requirements.txt'):
-            subprocess.run(['pip', 'install', '-r', filename])
-        if dev:
-            for filename in glob.iglob('metadash/plugins/*/requirements.dev.txt'):
+        if inst_py_dep:
+            info("*** Installing requirements of Metadash ... ***")
+            subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
+            if dev:
+                subprocess.run(['pip', 'install', '-r', 'requirements.dev.txt'])
+            info("*** Installing requirements of Metadash Done ***")
+
+            info("*** Installing requirements of Metadash Plugins ... ***")
+            for filename in glob.iglob('metadash/plugins/*/requirements.txt'):
                 subprocess.run(['pip', 'install', '-r', filename])
-        info("*** Installing requirements of Metadash Plugins Done ***")
+            if dev:
+                for filename in glob.iglob('metadash/plugins/*/requirements.dev.txt'):
+                    subprocess.run(['pip', 'install', '-r', filename])
+            info("*** Installing requirements of Metadash Plugins Done ***")
 
-        info("*** Install node packages ***")
-        if dev:
-            subprocess.run(['npm', 'install'])
-        else:
-            subprocess.run(['npm', 'install', '--production'])
-        info("*** Install node packages Done ***")
+        if inst_node_dep:
+            info("*** Install node packages ***")
+            if dev:
+                subprocess.run(['npm', 'install'])
+            else:
+                subprocess.run(['npm', 'install', '--production'])
+            info("*** Install node packages Done ***")
 
-        if only_dep:
-            info("*** Only install dependencies, exiting ***")
-            sys.exit(0)
-
-        info("*** Building Asserts ***")
-        subprocess.run(['npm', 'run', 'build'])
-        info("*** Building Asserts Done ***")
-        sys.exit(0)
+        if build_assert:
+            info("*** Building Asserts ***")
+            subprocess.run(['npm', 'run', 'build'])
+            info("*** Building Asserts Done ***")
 
     except Exception as err:
         error("Failed with exception:")
         raise
 
 
-try:
-    _script, cmd, *args = sys.argv
-    if cmd == 'initialize':
-        dev, only_dep = False, False
-        for arg in args:
-            if arg == '--dev':
-                dev = True
-            elif arg == '--only-dependency':
-                only_dep = True
-            else:
-                error('Unknown arg {}'.format(arg))
-        do_initialize(dev, only_dep)
-except ValueError:
-    # No args
-    # Supposed to print an help info help, just leave it to flask-manager
-    pass
+if __name__ == 'main':
+    try:
+        _script, cmd, *args = sys.argv
+        if cmd == 'initialize':
+            dev, only_dep, only_build = False, False, False
+            for arg in args:
+                if arg == '--dev':
+                    dev = True
+                elif arg == '--only-dependency':
+                    only_dep = True
+                elif arg == '--only-build':
+                    only_build = True
+                else:
+                    error('Unknown arg {}'.format(arg))
+            do_initialize(dev,
+                          not only_build,
+                          not only_build,
+                          not only_dep)
+    except ValueError:
+        # No args
+        # Supposed to print an help info help, just leave it to flask-manager
+        pass
 
+    try:
+        import coloredlogs
+        coloredlogs.install(level='INFO')
 
-try:
-    import coloredlogs
-    coloredlogs.install(level='INFO')
+        # Load Flask
+        from metadash import app, db  # noqa
 
-    # Load Flask
-    from metadash import app, db  # noqa
+        # Load Manager and Migration
+        from flask_migrate import Migrate, MigrateCommand  # noqa
+        from flask_script import Manager  # noqa
 
-    # Load Manager and Migration
-    from flask_migrate import Migrate, MigrateCommand  # noqa
-    from flask_script import Manager  # noqa
+        migrate = Migrate(app, db)
+        manager = Manager(app)
+        manager.add_command('db', MigrateCommand)
+    except ImportError:
+        error('Failed with following exception, please'
+              'make sure you have run "manager.py initialize first"!')
+        raise
 
-    migrate = Migrate(app, db)
-    manager = Manager(app)
-    manager.add_command('db', MigrateCommand)
-except ImportError:
-    error('Failed with following exception, please'
-          'make sure you have run "manager.py initialize first"!')
-    raise
+    @manager.command
+    @manager.option('-d', '--develop', dest='dev')
+    @manager.option('-o', '--only-dependency', dest='only_dep')
+    @manager.option('-b', '--only-build', dest='only_build')
+    def initialize(dev=False, only_dep=False, only_build=False):
+        do_initialize(dev,
+                      not only_build,
+                      not only_build,
+                      not only_dep)
 
+    @manager.command
+    @manager.option('-t', '--testcase', dest='testcase')
+    def test_api(testcase=None):
+        import unittest
+        import os
+        from metadash import plugins
 
-@manager.command
-@manager.option('-d', '--develop', dest='dev')
-@manager.option('-o', '--only-dependency', dest='only_dep')
-def initialize(dev=False, only_dep=False):
-    initialize(dev, only_dep)
+        suite = unittest.TestSuite()
+        loader = unittest.loader.defaultTestLoader
 
-
-@manager.command
-@manager.option('-t', '--testcase', dest='testcase')
-def test_api(testcase=None):
-    import unittest
-    import os
-    from metadash import plugins
-
-    suite = unittest.TestSuite()
-    loader = unittest.loader.defaultTestLoader
-
-    if testcase:
-        try:
-            suite.addTests(loader.loadTestsFromName(testcase))
-        except Exception:
-            return "Unable to load given test case"
-        else:
-            pass
-    else:
-        suite.addTests(loader.loadTestsFromName("metadash.test.api"))
-
-        for file in os.listdir('metadash/test/api'):
-            if not file.startswith('_') and not file.startswith('.') and file.endswith('.py'):
-                suite.addTests(loader.loadTestsFromName("metadash.test.api.{}".format(file[:-3])))
-
-        for plugin_name, plugin in plugins.get_all().items():
+        if testcase:
             try:
-                __import__(plugin['import'] + '.tests')
+                suite.addTests(loader.loadTestsFromName(testcase))
             except Exception:
-                error("Unable to load test case for plugin {}".format(plugin_name))
+                return "Unable to load given test case"
             else:
-                suite.addTests(loader.loadTestsFromName(plugin['import'] + '.tests'))
+                pass
+        else:
+            suite.addTests(loader.loadTestsFromName("metadash.test.api"))
 
-    runner = unittest.TextTestRunner()
-    result = runner.run(suite)
-    return len(result.errors + result.failures)
+            for file in os.listdir('metadash/test/api'):
+                if not file.startswith('_') and not file.startswith('.') and file.endswith('.py'):
+                    suite.addTests(loader.loadTestsFromName("metadash.test.api.{}".format(file[:-3])))
 
+            for plugin_name, plugin in plugins.get_all().items():
+                try:
+                    __import__(plugin['import'] + '.tests')
+                except Exception:
+                    error("Unable to load test case for plugin {}".format(plugin_name))
+                else:
+                    suite.addTests(loader.loadTestsFromName(plugin['import'] + '.tests'))
 
-@manager.command
-@manager.option('-n', '--name', dest='name')
-@manager.option('-p', '--password', dest='password')
-@manager.option('-r', '--role', dest='role')
-def create_user(username=None, password=None, role="admin"):
-    """
-    Create or overlay a user with local authentication
-    """
-    if username and password:
-        from metadash.auth import user_signup, user_setrole
-        user_signup(username, password, 'local')
-        user_setrole(username, role)
-    else:
-        raise "Username and password required"
+        runner = unittest.TextTestRunner()
+        result = runner.run(suite)
+        return len(result.errors + result.failures)
 
+    @manager.command
+    @manager.option('-n', '--name', dest='name')
+    @manager.option('-p', '--password', dest='password')
+    @manager.option('-r', '--role', dest='role')
+    def create_user(username=None, password=None, role="admin"):
+        """
+        Create or overlay a user with local authentication
+        """
+        if username and password:
+            from metadash.auth import user_signup, user_setrole
+            user_signup(username, password, 'local')
+            user_setrole(username, role)
+        else:
+            raise "Username and password required"
 
-@manager.command
-def create_database():
-    with app.app_context():
-        db.create_all()
+    @manager.command
+    def create_database():
+        with app.app_context():
+            db.create_all()
 
-
-# Start the manager
-if __name__ == '__main__':
     manager.run()
---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# Pack it as a package for some rare case usage
+# Be sure to install only in a virtualenv
+# It is not supposed to be install as a common package at all
+
+import glob
+from setuptools import setup, find_packages
+from manager import do_initialize
+
+
+def read_file(file_name):
+    with open(file_name) as file:
+        return file.read()
+
+
+def find_all_requirements(develop=False):
+    requirements = {read_file('requirements.txt').splitlines()}
+    if develop:
+        requirements.update(read_file('requirements.dev.txt').splitlines())
+    for filename in glob.glob('metadash/plugins/*/requirements.txt'):
+        requirements.update(read_file(filename).splitlines())
+        if develop:
+            for filename in glob.glob('metadash/plugins/*/requirements.dev.txt'):
+                requirements.update(read_file(filename).splitlines())
+    return requirements
+
+
+setup_params = dict(
+    name='metadash',
+    version='1.0',
+    author='Kairui Song',
+    author_email='ryncsn@gmail.com',
+    license='GPLv3',
+    python_requires='>=3.5',
+    packages=find_packages(),
+    package_data={
+        '': [
+            'config/*',
+            'dist/*',
+            'dist/*/*',
+            'dist/*/*/*',
+            'plugins/*/*.json',
+        ],
+    },
+    scripts=[
+        'bin/md-manager',
+    ],
+)
+
+
+if __name__ == '__main__':
+    do_initialize(dev=False, inst_py_dep=False, inst_node_dep=True, build_assert=True)
+    setup(**setup_params)
