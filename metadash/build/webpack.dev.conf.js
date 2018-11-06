4e741569810bb1b37e49ee9a8f4f6758b9e65e60
ryncsn <ryncsn@gmail.com>
2018-11-06 20:35:33 +0800

    Fix ui-devel server error
    Also add comment about usage of bin/ui-server

--- metadash/build/webpack.dev.conf.js
+++ metadash/build/webpack.dev.conf.js
@@ -26,7 +26,7 @@ module.exports = merge(baseWebpackConfig, {
     new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
-      filename: 'src/index.html',
+      filename: 'index.html',
       template: 'src/index.html',
       inject: true
     }),
